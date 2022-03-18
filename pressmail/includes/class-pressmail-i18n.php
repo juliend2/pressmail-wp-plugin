2805178aebb4780189d48e77c1589a1fd70e8d22
Julien Desrosiers <jdesrosiers@audiokinetic.com>
2022-03-18 18:08:54 -0400

    replace Plugin Name by Pressmail

--- pressmail/includes/class-pressmail-i18n.php
+++ pressmail/includes/class-pressmail-i18n.php
@@ -9,8 +9,8 @@
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/includes
+ * @package    Pressmail
+ * @subpackage Pressmail/includes
  */
 
 /**
@@ -20,11 +20,11 @@
  * so that it is ready for translation.
  *
  * @since      1.0.0
- * @package    Plugin_Name
- * @subpackage Plugin_Name/includes
+ * @package    Pressmail
+ * @subpackage Pressmail/includes
  * @author     Your Name <email@example.com>
  */
-class Plugin_Name_i18n {
+class Pressmail_i18n {
 
 
 	/**
@@ -35,7 +35,7 @@ class Plugin_Name_i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'plugin-name',
+			'pressmail',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
--- pressmail/uninstall.php
+++ pressmail/uninstall.php
@@ -22,7 +22,7 @@
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
+ * @package    Pressmail
  */
 
 // If uninstall not called from WordPress, then exit.
